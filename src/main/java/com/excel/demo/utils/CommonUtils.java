60e9b580d1b6dfb95882567257e43b47ac1fb4b6
ZhuWenHua <zhu.wenhua@sz-excel.com>
2018-12-25 11:39:34 +0800

    use opencsv

--- src/main/java/com/excel/demo/utils/CommonUtils.java
+++ src/main/java/com/excel/demo/utils/CommonUtils.java
@@ -19,6 +19,7 @@ import org.springframework.context.annotation.Configuration;
 import com.excel.demo.bean.Deposit;
 import com.excel.demo.bean.Loan;
 import com.excel.demo.bean.RateInfo;
+import com.opencsv.CSVReader;
 
 @Configuration
 public class CommonUtils {
@@ -137,6 +138,27 @@ public class CommonUtils {
 		}
 	}
 	
+	
+	/**
+	 * Decompose reply file for details
+	 * 
+	 * @param obj
+	 * @param as_Filename
+	 * @param a_Lst
+	 */
+	public void getDetailByFile(Object obj, String as_Filename, List a_Lst) {
+		
+		if(obj instanceof RateInfo   ) {
+			getRateDetailByFile((RateInfo)obj, as_Filename, a_Lst);
+		}else if(obj instanceof Loan) {
+			getLoanDetailByFile((Loan)obj, as_Filename, a_Lst);
+		}else if(obj instanceof Deposit) {
+			getDepositDetailByFile((Deposit)obj, as_Filename, a_Lst);
+		}
+
+	}
+	
+	
 	/**
 	 * Decompose reply file for product ID
 	 * 
@@ -145,6 +167,237 @@ public class CommonUtils {
 	 * @param as_Filename
 	 * @param obj
 	 */
+	public void getProdsByFile(List a_ProdLst, String as_Filename, Object obj) {
+		CSVReader reader = null;
+		String[] ls_NextLine;
+		try{
+			String ls_localInpath = null;
+			if(!localInpath.endsWith(File.separator)) {
+				ls_localInpath = localInpath + File.separator + as_Filename + "." + remoteInFileConv;
+			}else {
+				ls_localInpath = localInpath + as_Filename + "." + remoteInFileConv;;
+			}
+			LOGGER.info("ls_localInpath " + ls_localInpath);
+			
+			reader = new CSVReader(new FileReader(ls_localInpath));
+			while((ls_NextLine=reader.readNext()) != null) {
+				LOGGER.info("ls_NextLine.length : " +ls_NextLine.length);
+				if(obj instanceof Loan) {
+					((Loan) obj).setprodId(ls_NextLine[0]);
+					a_ProdLst.add(obj);
+					obj = new Loan();
+				}else if (obj instanceof Deposit) {
+					((Deposit) obj).setprodId(ls_NextLine[0]);
+					a_ProdLst.add(obj);
+					obj = new Deposit();
+				}
+			}
+		}catch(FileNotFoundException ex) {
+			LOGGER.error("File " + as_Filename +" not found!" );
+			return;
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}finally{
+			if(reader != null){
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	
+	}
+
+	/**
+	 * Decompose reply file for Deposit details
+	 * 
+	 * @param info
+	 * @param as_Filename
+	 * @param a_Lst
+	 */
+	public void getDepositDetailByFile(Deposit info, String as_Filename, List a_Lst) {
+		
+		CSVReader reader = null;
+		String[] ls_NextLine;
+		try{
+			String ls_localInpath = null;
+			if(!localInpath.endsWith(File.separator)) {
+				ls_localInpath = localInpath + File.separator + as_Filename + "." + remoteInFileConv;;
+			}else {
+				ls_localInpath = localInpath + as_Filename + "." + remoteInFileConv;;
+			}
+			LOGGER.info("ls_localInpath " + ls_localInpath);
+			reader = new CSVReader(new FileReader(ls_localInpath));
+			while((ls_NextLine=reader.readNext()) != null) {
+				LOGGER.info("ls_NextLine.length : " +ls_NextLine.length);
+				//info
+				if(info.getprodId() == null)
+						info.setprodId(ls_NextLine[0]);
+				if (ls_NextLine.length > 2)
+					info.setProduct(ls_NextLine[1]);
+				if (ls_NextLine.length > 2)
+					info.setType(ls_NextLine[2]);
+				if (ls_NextLine.length > 3)
+					info.setSubtype(ls_NextLine[3]);
+				if (ls_NextLine.length > 4)
+					info.setCurrency(ls_NextLine[4]);
+				if (ls_NextLine.length > 5)
+					info.setInterestRate(ls_NextLine[5]);
+				if (ls_NextLine.length > 6)
+					info.setMinamount(ls_NextLine[6]);
+				if (ls_NextLine.length > 7)
+					info.setFee(ls_NextLine[7]);
+				if (ls_NextLine.length > 8)
+					info.setRemark(ls_NextLine[8]);
+				a_Lst.add(info);
+				info = new Deposit();
+			}
+		}catch(FileNotFoundException ex) {
+			LOGGER.error("File " + as_Filename +" not found!" );
+			return;
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}finally{
+			if(reader != null){
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Decompose reply file for Loan details
+	 * 
+	 * @param info
+	 * @param as_Filename
+	 * @param a_Lst
+	 */
+	public void getLoanDetailByFile(Loan info, String as_Filename, List a_Lst) {
+		
+		CSVReader reader = null;
+		String[] ls_NextLine;
+		try{
+			String ls_localInpath = null;
+			if(!localInpath.endsWith(File.separator)) {
+				ls_localInpath = localInpath + File.separator + as_Filename + "." + remoteInFileConv;;
+			}else {
+				ls_localInpath = localInpath + as_Filename + "." + remoteInFileConv;;
+			}
+			LOGGER.info("ls_localInpath " + ls_localInpath);
+			reader = new CSVReader(new FileReader(ls_localInpath));
+			while((ls_NextLine=reader.readNext()) != null) {
+				LOGGER.info("ls_NextLine.length : " +ls_NextLine.length);
+				if(info.getprodId() == null)
+					info.setprodId(ls_NextLine[0]);
+				if (ls_NextLine.length > 1)
+					info.setProduct(ls_NextLine[1]);
+				if (ls_NextLine.length > 2)	
+					info.setType(ls_NextLine[2]);
+				if (ls_NextLine.length > 3)					
+					info.setSubtype(ls_NextLine[3]);
+				if (ls_NextLine.length > 4)
+					info.setInterestRate(ls_NextLine[4]);
+				if (ls_NextLine.length > 5)
+					info.setPrdinfo1(ls_NextLine[5]);
+				if (ls_NextLine.length > 6)	
+					info.setPrdinfo2(ls_NextLine[6]);
+				if (ls_NextLine.length > 7)
+					info.setPrdinfo3(ls_NextLine[7]);
+				if (ls_NextLine.length > 8)
+					info.setFee(ls_NextLine[8]);
+				if (ls_NextLine.length > 9)
+					info.setRemark(ls_NextLine[9]);
+				a_Lst.add(info);
+				info = new Loan();
+			}
+		}catch(FileNotFoundException ex) {
+			LOGGER.error("File " + as_Filename +" not found!" );
+			return;
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}finally{
+			if(reader != null){
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Decompose reply file for Rate details
+	 * 
+	 * @param info
+	 * @param as_Filename
+	 * @param a_Lst
+	 */
+	public void getRateDetailByFile(RateInfo info, String as_Filename, List a_Lst) {
+		
+		CSVReader reader = null;
+		String[] ls_NextLine;
+		try{
+			String ls_localInpath = null;
+			if(!localInpath.endsWith(File.separator)) {
+				ls_localInpath = localInpath + File.separator + as_Filename + "." + remoteInFileConv;;
+			}else {
+				ls_localInpath = localInpath + as_Filename + "." + remoteInFileConv;;
+			}
+			LOGGER.info("ls_localInpath " + ls_localInpath);
+			reader = new CSVReader(new FileReader(ls_localInpath));
+			
+			String ls_Ccy = info.getCcy_Cde();
+			String ls_RelvtCcy = info.getRelvt_Ccy_Cde();
+			while((ls_NextLine=reader.readNext()) != null) {
+				LOGGER.info("ls_NextLine.length : " +ls_NextLine.length);
+				//info
+				if(info.getCcy_Cde() == null) {
+					info.setCcy_Cde(ls_Ccy);
+					info.setRelvt_Ccy_Cde(ls_RelvtCcy);
+				}
+				if (ls_NextLine.length > 1)
+					info.setBid(new BigDecimal(ls_NextLine[1]));
+				if (ls_NextLine.length > 2)
+					info.setMid(new BigDecimal(ls_NextLine[2]));
+				if (ls_NextLine.length > 3)
+					info.setAsk(new BigDecimal(ls_NextLine[3]));
+				if (ls_NextLine.length > 4)
+					info.setFeed_Source(ls_NextLine[4]);
+				if (ls_NextLine.length > 5)
+					info.setLastDate(ls_NextLine[5]);
+				a_Lst.add(info);
+				info = new RateInfo();
+			}
+		}catch(FileNotFoundException ex) {
+			LOGGER.error("File " + as_Filename +" not found!" );
+			return;
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}finally{
+			if(reader != null){
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+/*	
+	*//**
+	 * Decompose reply file for product ID
+	 * 
+	 * 
+	 * @param a_ProdLst
+	 * @param as_Filename
+	 * @param obj
+	 *//*
 	public void getProdsByFile(List a_ProdLst, String as_Filename, Object obj) {
 		BufferedReader l_br = null;
 		try{
@@ -190,33 +443,13 @@ public class CommonUtils {
 	
 	}
 
-	/**
-	 * Decompose reply file for details
-	 * 
-	 * @param obj
-	 * @param as_Filename
-	 * @param a_Lst
-	 */
-	public void getDetailByFile(Object obj, String as_Filename, List a_Lst) {
-		
-		if(obj instanceof RateInfo   ) {
-			getRateDetailByFile((RateInfo)obj, as_Filename, a_Lst);
-		}else if(obj instanceof Loan) {
-			getLoanDetailByFile((Loan)obj, as_Filename, a_Lst);
-		}else if(obj instanceof Deposit) {
-			getDepositDetailByFile((Deposit)obj, as_Filename, a_Lst);
-		}
-
-	}
-
-	
-	/**
+	*//**
 	 * Decompose reply file for Deposit details
 	 * 
 	 * @param info
 	 * @param as_Filename
 	 * @param a_Lst
-	 */
+	 *//*
 	public void getDepositDetailByFile(Deposit info, String as_Filename, List a_Lst) {
 		
 		BufferedReader l_br = null;
@@ -272,13 +505,13 @@ public class CommonUtils {
 		}
 	}
 	
-	/**
+	*//**
 	 * Decompose reply file for Loan details
 	 * 
 	 * @param info
 	 * @param as_Filename
 	 * @param a_Lst
-	 */
+	 *//*
 	public void getLoanDetailByFile(Loan info, String as_Filename, List a_Lst) {
 		
 		BufferedReader l_br = null;
@@ -335,13 +568,13 @@ public class CommonUtils {
 		}
 	}
 	
-	/**
+	*//**
 	 * Decompose reply file for Rate details
 	 * 
 	 * @param info
 	 * @param as_Filename
 	 * @param a_Lst
-	 */
+	 *//*
 	public void getRateDetailByFile(RateInfo info, String as_Filename, List a_Lst) {
 		
 		BufferedReader l_br = null;
@@ -394,6 +627,5 @@ public class CommonUtils {
 				}
 			}
 		}
-	}
-
+	}*/
 }
